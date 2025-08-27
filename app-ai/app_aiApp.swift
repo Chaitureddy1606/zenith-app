a66f56d99d77cfee6d5926dec1353487bb2b6fa1
chaitu <mulechaitu3@gmail.com>
2025-08-27 15:09:08 +0530

    Initial Commit

---
+++ app-ai/app_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  app_aiApp.swift
+//  app-ai
+//
+//  Created by chaitu  on 27/08/25.
+//
+
+import SwiftUI
+
+@main
+struct app_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app-aiTests/app_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  app_aiTests.swift
+//  app-aiTests
+//
+//  Created by chaitu  on 27/08/25.
+//
+
+import Testing
+@testable import app_ai
+
+struct app_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
